b6bb5fc4fe774abe7befa2706232851629de65bc
Kevin Evans <kwevans@gmail.com>
2024-06-27 21:28:51 +0100

    Initial Commit

---
+++ posty/postyApp.swift
@@ -0,0 +1,17 @@
+//
+//  postyApp.swift
+//  posty
+//
+//  Created by Kevin Evans on 27/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct postyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
